b4f02757286e70951e383d113441e6756a47a54e
Richard Ditshai <richardmckid@gmail.com>
2022-06-07 22:40:07 +0200

    fixed middleware and user role

--- routes/auth.js
+++ routes/auth.js
@@ -45,16 +45,17 @@ authRouter.post('/login', async(req, res) =>{
         const token = jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET, {expiresIn: '1h'})
         const {password, ...currentUser} = user._doc
 
-        res.cookie(
-            'access-token', token, {
-                secure: 'development',
-                httpOnly: true,
-                maxAge: 24 * 60 * 60 * 1000
-            }
-        )
+        // res.cookie(
+        //     'access-token', token, {
+        //         secure: 'development',
+        //         httpOnly: true,
+        //         maxAge: 24 * 60 * 60 * 1000
+        //     }
+        // )
         res.status(200).json({
             success: true,
             currentUser,
+            token
         })
 
     } catch (error) {
